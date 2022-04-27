0a70870040699e54633c7c1c048316de2dbdb46f
bfreebnf12 <50027898+bfreebnf12@users.noreply.github.com>
2022-04-27 14:03:10 -0400

    adding files to github

---
+++ app.js
@@ -0,0 +1,30 @@
+//set inital count
+let count = 0;
+//select value amd buttons
+const value = document.getElementById("value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function(btn) {
+    btn.addEventListener("click", clickHandler);
+})
+
+function clickHandler(e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+        count--;
+    } else if (styles.contains("increase")) {
+        count++;
+    } else {
+        count = 0;
+    }
+    if (count > 0) {
+        value.style.color = "green";
+    }
+    if (count < 0) {
+        value.style.color = "red";
+    }
+    if (count === 0) {
+        value.style.color = "#222";
+    }
+    value.innerText = count;
+}
